73018ee159ee7fd04a2804893df81eefb1e30390
DimatiBart <dimati.bart@gmail.com>
2017-06-07 17:05:57 +0300

    es2015 for life

--- src/actions/blogModule.js
+++ src/actions/blogModule.js
@@ -1,16 +1,16 @@
 export const LOAD_DATA = "LOAD_DATA";
 export const DATA_LOADED = "LOADED_DATA";
 
-export let loadData = (url) => {
-    return {
+export let loadData = (url) => (
+    {
         type: LOAD_DATA,
         url
     }
-}
+);
 
-export let dataLoaded = (posts) => {
-    return {
+export let dataLoaded = (posts) => (
+    {
         type: DATA_LOADED,
         posts
     }
-}
+);
